290e5ebaa7b0cb62c3f5cd75e7d2a831dc4a00a2
baijianmin <baijianmin@costrip.cn>
2023-09-01 15:01:58 +0800

    dbb-grpc模块初始化

--- dbb-grpc/src/main/java/top/dabaibai/grpc/GrpcApplication.java
+++ dbb-grpc/src/main/java/top/dabaibai/grpc/GrpcApplication.java
@@ -1,12 +1,18 @@
 package top.dabaibai.grpc;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @description:
+ * @description: gRPC启动类
  * @author: 白剑民
  * @dateTime: 2023/9/1 14:44
  */
-@Slf4j
+@SpringBootApplication
 public class GrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcApplication.class, args);
+    }
+
 }
