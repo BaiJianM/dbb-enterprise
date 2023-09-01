15dc8c9272e712e8844f3b248fb0d9734b386723
BaiJianM <13691762041@163.com>
2023-09-01 14:44:31 +0800

    增加dbb-grpc模块

---
+++ dbb-grpc/src/main/java/top/dabaibai/grpc/GrpcApplication.java
@@ -0,0 +1,12 @@
+package top.dabaibai.grpc;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @description:
+ * @author: 白剑民
+ * @dateTime: 2023/9/1 14:44
+ */
+@Slf4j
+public class GrpcApplication {
+}
